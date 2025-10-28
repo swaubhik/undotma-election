4b9aee884884696928051f2caf891eaba45a29ac
swaubhik <p22cse1008@cit.ac.in>
2025-10-28 11:02:22 +0530

    Fix redirect route in authentication store method to point to admin dashboard

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('admin', absolute: true));
+        return redirect()->intended(route('admin.dashboard', absolute: true));
     }
 
     /**
